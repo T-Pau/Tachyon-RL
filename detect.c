789e5a2b891c7cdbe9c711065859ac7294204036
Dieter Baron <dillo@nih.at>
2020-09-13 10:55:29 +0200

    Improve user interface.

--- detect.c
+++ detect.c
@@ -1,5 +1,8 @@
 #include "tachyon.h"
 
+#include <c64.h>
+#include <conio.h>
+#include <ctype.h>
 #include <stdio.h>
 
 #include "ramlink/ramlink.h"
@@ -10,39 +13,108 @@ unsigned char dos;
 unsigned long ramlink_size;
 unsigned long reu_size;
 
+#define OK 0
+#define WARNING 1
+#define ERROR 2
+
 unsigned char detect(void) {
     const char *string;
-    unsigned char ok = 1;
+    unsigned char state = OK;
+    unsigned char key;
 
 	printf("RAMLink:  ");
 	if ((ramlink_size = ramlink_get_size()) == 0) {
+        textcolor(COLOR_LIGHTRED);
 		printf("not found\n");
-        ok = 0;
+        textcolor(COLOR_GRAY3);
+        state = ERROR;
 	}
     else {
+        textcolor(COLOR_LIGHTGREEN);
     	printf("%lu bytes\n", ramlink_size);
+        textcolor(COLOR_GRAY3);
     }
 
 	printf("REU:      ");
 	if ((reu_size = (unsigned long)reu_detect() << 16) == 0) {
+        textcolor(COLOR_YELLOW);
 		printf("not found\n");
+        textcolor(COLOR_GRAY3);
+        if (state == OK) {
+            state = WARNING;
+        }
 	}
 	else {
+        textcolor(COLOR_LIGHTGREEN);
 		printf("%lu bytes\n", reu_size);
+        textcolor(COLOR_LIGHTGREEN);
 	}
 
 	printf("Ultimate: ");
 	if ((string = ultimate_dos_identify(1)) == NULL) {
+        textcolor(COLOR_LIGHTRED);
 		printf("not found\n");
-        ok = 0;
+        textcolor(COLOR_GRAY3);
+        state = ERROR;
 	}
     else {
-    	printf("%s\n\n", string);
+        textcolor(COLOR_LIGHTGREEN);
+    	printf("%s\n", string);
+        textcolor(COLOR_GRAY3);
     }
+    printf("\n");
 
-    if (!ok) {
-        return 1;
+#if ENABLE_RAMLINK
+    if (ramlink_size == 0) {
+        printf("Please make sure your RAMLink\n");
+        printf("is connected and enabled.\n\n");
     }
+#endif
+    
+#if ENABLE_DOS
+    if (dos == 0) {
+        printf("Please make sure your Ultimate is\n");
+        printf("connected to the Pass Through port of\n");
+        printf("your RAMLink and that the Command\n");
+        printf("Interface is enabled: Press the Menu\n");
+        printf("button, then F2. Select \"C64 and\n");
+        printf("Cartridge Settings\" and enable\n");
+        printf("\"Command Interface\".\n\n");
+    }
+#endif
+#if ENABLE_REU
+    if (reu_size == 0) {
+        printf("Tachyon RL is much faster if it can\n");
+        printf("use the Ultimate's RAM Expansion Unit.\n");
+        printf("Press the Menu button, then F2. Select\n");
+        printf("\"C64 and Cartridge Settings\" and\n");
+        printf("enable \"RAM Expansion Unit\". A REU\n");
+        printf("Size of 16 MB is best.\n\n");
+    }
+#endif
 
-    return 0;
+    switch (state) {
+    case OK:
+        return 0;
+        
+    case WARNING:
+        printf("(C)ontinue, (R)echeck, or (H)elp.\n");
+        do {
+            key = tolower(cgetc());
+        } while (key != 'c' && key != 'r' && key != 'h');
+        if (key == 'c') {
+            return 0;
+        }
+        break;
+        
+    case ERROR:
+        printf("(H)elp or any other key to recheck.");
+        key = tolower(cgetc());
+        break;
+    }
+    
+    if (key == 'h') {
+        help();
+    }
+    return 1;
 }
--- main.c
+++ main.c
@@ -12,78 +12,77 @@ int main(void) {
 	const char *string;
 	unsigned int ret;
 
-    bgcolor(COLOR_BLACK);
-    bordercolor(COLOR_BLACK);
-    textcolor(COLOR_GRAY3);
-    clrscr();
-    textcolor(COLOR_WHITE);
-    printf("Tachyon RL - Back Up Your RAMLink\n\n");
-    textcolor(COLOR_GRAY3);
-
-#if 0
-	if (detect() != 0) {
-		return 0;
-	}
-#endif
-
-	string = ultimate_dos_copy_home_path(1);
-	printf("Current directory:\n  %s\n", string);
-
-    do {
-        gotox(0);
-        printf("B)ackup, R)estore, or H)elp");
-        ret = tolower(cgetc());
-    } while (ret != 'b' && ret != 'r' && ret != 'h');
-    printf("\n");
-    
-    if (ret == 'h') {
-        help();
-        /* TODO: back to beginning */
-        return 1;
-    }
-    
-	printf("Filename: ");
-	fgets(filename, 256, stdin);
-	filename[strlen(filename)-1] = '\0';
-    
-    if (ret == 'B' || ret == 'b') {
-        timer_start();
-        ret = backup();
-        timer_stop();
+    while (1) {
+        bgcolor(COLOR_BLACK);
+        bordercolor(COLOR_BLACK);
+        textcolor(COLOR_GRAY3);
+        clrscr();
+        textcolor(COLOR_WHITE);
+        printf("Tachyon RL - Back up RAMLink to Ultimate\n");
+        textcolor(COLOR_GRAY3);
         
-        if (ret == 0) {
-            puts("Backup done in ");
-            timer_output();
-            puts("\n");
+        if (detect() != 0) {
+            continue;
+        }
+        
+        string = ultimate_dos_copy_home_path(1);
+        printf("Current directory:\n  %s\n", string);
+        
+        printf("(B)ackup, (R)estore, or (H)elp.\n");
+        do {
+            ret = tolower(cgetc());
+        } while (ret != 'b' && ret != 'r' && ret != 'h');
+        
+        if (ret == 'h') {
+            help();
+            continue;
+        }
+        
+        printf("Filename: ");
+        fgets(filename, 256, stdin);
+        filename[strlen(filename)-1] = '\0';
+        
+        if (ret == 'B' || ret == 'b') {
+            timer_start();
+            ret = backup();
+            timer_stop();
+            
+            if (ret == 0) {
+                printf("Backup done in ");
+                timer_output();
+                printf("\n");
+            }
+            else {
+                printf("Backup failed.\n");
+            }
         }
         else {
-            puts("Backup failed.\n");
-        }
-    }
-    else {
-        printf("You are about to restore RAMLink.\n");
-        printf("This will erase its current contents.\n");
-        printf("Are you sure? ");
-        ret = cgetc();
-        printf("%c\n", ret);
-        if (ret != 'Y' && ret != 'y') {
-            return 0;
+            printf("You are about to restore RAMLink.\n");
+            printf("This will erase its current contents.\n");
+            printf("Are you sure (y/n)?\n");
+            ret = tolower(cgetc());
+            if (ret != 'Y') {
+                continue;
+            }
+            
+            timer_start();
+            ret = restore();
+            timer_stop();
+            
+            if (ret == 0) {
+                printf("Restore done in ");
+                timer_output();
+                printf("\n");
+                /* TODO: re-init RAMLink? power cycle? */
+            }
+            else {
+                printf("Restore failed.\n");
+            }
         }
         
-        timer_start();
-        ret = restore();
-        timer_stop();
-        
-        if (ret == 0) {
-            puts("Restore done in ");
-            timer_output();
-            puts("\n");
-            /* TODO: re-init RAMLink? power cycle? */
-        }
-        else {
-            puts("Restore failed.\n");
-        }
+        printf("Press any key to continue.\n");
+        cgetc();
     }
-
+    
     return 0;
 }
